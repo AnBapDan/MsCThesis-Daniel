96dfe58975659443df5e0882b3e1aa083182d671
Daniel Andrade <44200893+AnBapDan@users.noreply.github.com>
2023-06-01 19:12:41 +0100

    Embedded client on function
    IT does not work with outside client file

--- device_scripts/transactions/hedera/sendTransaction.ts
+++ device_scripts/transactions/hedera/sendTransaction.ts
@@ -1,20 +1,24 @@
 import {
-    AccountBalanceQuery,
     AccountId,
-    AccountUpdateTransaction,
     Client,
     Hbar,
     HbarUnit,
     PrivateKey,
-    TransactionReceiptQuery,
-    TransactionRecordQuery,
     TransferTransaction,
 } from "@hashgraph/sdk";
-import { ed25519_client } from "./operator";
 
 export async function handleJsonPayment(entry: any) {
+    const mirror = "10.255.33.18:5600"
+
+    const node = { "10.255.33.18:50211": new AccountId(3) };
+    const ed25519_client = Client.forNetwork(node).setMirrorNetwork(mirror).setMaxAttempts(1000000);
+    ed25519_client.setOperator(
+        AccountId.fromString("0.0.1022"),
+        PrivateKey.fromStringED25519("0xa608e2130a0a3cb34f86e757303c862bee353d9ab77ba4387ec084f881d420d4")
+    );
+
     const { id, identifier, buyer, seller, energy, price, timestamp } = entry;
-    
+
     const transaction = await new TransferTransaction()
         //THIS FIRST PART NEED REFACTOR ON DEPLOY
         .addHbarTransfer(ed25519_client.operatorAccountId!, Hbar.from(-Math.abs(price), HbarUnit.Tinybar))
@@ -23,8 +27,8 @@ export async function handleJsonPayment(entry: any) {
         .execute(ed25519_client);
     const transactionReceipt = await transaction.getReceipt(ed25519_client);
     console.log(`[${new Date()}] Transaction "${transaction.transactionId.toString()}" with status: ${transactionReceipt.status.toString()} `)
-    if(transactionReceipt.status.toString()==="SUCCESS"){
+    if (transactionReceipt.status.toString() === "SUCCESS") {
         return transaction.transactionId.toString()
     }
-   
+
 }
