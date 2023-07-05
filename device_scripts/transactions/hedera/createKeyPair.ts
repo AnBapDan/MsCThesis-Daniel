dd650656c10c4aabbd8b4d0148a2795dbae18b2d
Daniel Andrade <44200893+AnBapDan@users.noreply.github.com>
2023-07-05 18:49:51 +0100

    Update and rename createKeyPair to createKeyPair.ts

--- device_scripts/transactions/hedera/createKeyPair.ts
+++ device_scripts/transactions/hedera/createKeyPair.ts
@@ -5,4 +5,9 @@ export async function genkeys(){
   const publicKey = privateKey.publicKey;
   console.log("private key = " + privateKey);
   console.log("public key = " + publicKey);
+  return {publicKey, privateKey}
+}
+
+export async function getAccountId(pubkey:string){
+  const response = await axios.get(`http://10.255.33.18:5551/api/v1/accounts?account.publickey=${pubkey}`)
 }
